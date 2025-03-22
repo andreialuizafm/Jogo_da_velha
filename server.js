830a948f0233a070cd2fca8b5749572c8b677353
Andreia Luiza Mattos <71427210+andreialuizafm@users.noreply.github.com>
2025-03-22 15:01:44 +0000

    feat: criar endpoint novo usuario

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const {PrismaClient} = require("@prisma/client");
+const req = require("express/lib/request");
 const app = express();
+const prisma = new PrismaClient();
 const PORT = 8080;
 
 app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
     res.json({ message: "API funcionando corretamente!" });
+});
+
+app.post("/api/v1/usuarios",async (req, res) =>{
+    const {
+            nome,
+            email,
+            senha,
+            username,
+            avatar
+        } = req.body;
+
+    const usuario = await prisma.usuario.create({
+        data: {
+            nome,
+            email,
+            senha,
+            username,
+            avatar
+        }
+    });
+    res.status(201).json(usuario);
 });
 
 app.listen(PORT, () => {
