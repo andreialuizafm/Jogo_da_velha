ec75bfaa3083cbed760b1a59e44ebdc67c52f9e6
Andreia Luiza Mattos <71427210+andreialuizafm@users.noreply.github.com>
2025-04-05 13:03:40 +0000

    feat:Inclusão de tabela

--- server.js
+++ server.js
@@ -1,37 +1,69 @@
 const express = require("express");
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const req = require("express/lib/request");
+const res = require("express/lib/response");
+
 const app = express();
 const prisma = new PrismaClient();
+
 const PORT = 8080;
 
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
     res.json({ message: "API funcionando corretamente!" });
 });
 
-app.post("/api/v1/usuarios",async (req, res) =>{
-    const {
-            nome,
-            email,
-            senha,
-            username,
-            avatar
-        } = req.body;
+app.post("/api/v1/usuarios", async (req, res) =>{
+    const   {
+                nome, 
+                email, 
+                senha, 
+                username, 
+                avatar
+            } = req.body;
 
     const usuario = await prisma.usuario.create({
         data: {
-            nome,
-            email,
-            senha,
-            username,
+            nome, 
+            email, 
+            senha, 
+            username, 
             avatar
-        }
+        }        
     });
+
     res.status(201).json(usuario);
 });
 
+app.post('/api/v1/login', async(req, res) => {
+    const {email, senha }= req.body;
+    const usuario = await prisma.usuario.findUnique({where:{email, senha}});
+
+    if(usuario == null){
+        return res.status(401).json({error:"Credenciais Inválidas"});
+    }
+    else{
+        return res.status(200).json(usuario);
+    }
+})
+
+app.post("/api/v1/conexao", async(req,res)=>{
+    const {id_usuario_a,id_usuario_b} = req.body;
+    const conexao = await prisma.conexao.create({
+        data: {
+            id_usuario_a,
+            id_usuario_b
+
+        }
+    });
+
+    console.log(conexao);
+    res.status(201).json(conexao)
+};
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(req.body);
 });
